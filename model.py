a9d4508f2f042012a7fd39eb92a33bf2a40e1916
Aidan-Jared <46360020+Aidan-Jared@users.noreply.github.com>
2019-05-22 11:43:24 -0600

    changed col to factors and tried out balance class

--- model.py
+++ model.py
@@ -10,9 +10,13 @@ if __name__ == "__main__":
     df = pd.read_json("data/modcloth_final_data.json", lines=True)
     df.drop(["review_summary", "review_text", "user_name", "quality"], axis=1, inplace=True)
     fit = h2o.H2OFrame(df, na_strings=['NA', 'none', 'nan', "NaN"])
+    factor_col = ["fit", "bust", "category", "height", "length", "shoe size", "shoe width", "waist"]
+    for i in factor_col:
+        fit[i] = fit[i].asfactor()
     y = "fit"
     x = fit.columns
     del x[4]
+    del x[11]
     train, valid, text = fit.split_frame([.6,.2], seed=50)
     params = {
         'ntrees' : [10,20,30,40],
@@ -22,9 +26,9 @@ if __name__ == "__main__":
         'col_sample_rate' : [.9,.8],
         'seed' : [42],
         'stopping_rounds' : [5],
-        'stopping_tolerance' : [1e-3],
-        'stopping_metric' : ['mse']
-    }
+        'stopping_tolerance' : [1e-5],
+        'stopping_metric' : ['logloss'],
+        'balance_classes': [True, False]}
     grid = H2OGridSearch(H2OGradientBoostingEstimator, params, grid_id='gbm_grid_fit1')
     grid.train(x=x, y=y, training_frame=train, validation_frame=valid)
     print(grid)
